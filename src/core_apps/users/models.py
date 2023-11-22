0aa29905fef06e8c39bc277ce651b087f8fae916
torcuata22 <marilynjmarquez@gmail.com>
2023-11-22 10:41:47 -0500

    created custom user manager

--- src/core_apps/users/models.py
+++ src/core_apps/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionError):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+
