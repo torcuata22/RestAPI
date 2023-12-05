8442ed42800636c3a33eb0bc15cc087ca0f9bfe4
torcuata22 <marilynjmarquez@gmail.com>
2023-12-05 15:59:22 -0500

    add jwt authentication

--- src/authors_api/urls.py
+++ src/authors_api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from dj_rest_auth.views import PasswordResetConfirmView
+from core_apps.users.views import CustomUserDetailsView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,6 +22,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls), #used to be:settings.ADMIN_URL, which had route of "supersecret/" but this was causing problmes in nginx
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="redoc"),  # Add a name for the URL
+    path("api/v1/auth/user/", CustomUserDetailsView.as_view(), name="user_details"),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/auth/password/reset/confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 ]
 
 admin.site.site_header = "Authors Haven API Admin"
